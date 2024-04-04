aec8481aa81bcede78aaa16a21b81d339e4b2d0f
Jean Kuyk <jeankuyk7@gmail.com>
2024-04-04 10:40:06 -0500

    clone node pet card template

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// template and wrapper used in async func petsArea
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
+// display dynamic temperature in Miami
 async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
     const weatherData = await weatherPromise.json()
@@ -8,12 +13,17 @@ async function start() {
 
 start()
 
+// generate pet cards for each JSON object. Insert in list-of-pets div.
 async function petsArea() {
     const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
     const petsData = await petsPromise.json()
     petsData.forEach(pet => {
-        console.log(pet.name)
+        const clone = template.content.cloneNode(true)
+        clone.querySelector("h3").textContent = pet.name
+
+        wrapper.appendChild(clone)
     })
+    document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 
 petsArea()
